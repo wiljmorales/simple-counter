0de584e3b49d073c7e67825f8cf6bb2ad0792081
Wilfredo Morales <wiljmoralesa@gmail.com>
2022-05-26 22:53:26 +0000

    Challenge complete

--- src/js/component/App.js
+++ src/js/component/App.js
@@ -1,15 +1,14 @@
-import { counter } from "@fortawesome/fontawesome-svg-core";
 import React, { useState } from "react";
 import { Counter } from "./Counter";
 
 export const App = () => {
 	const [time, setTime] = useState(0);
-	let seconds = 0;
-	setInterval(() => {
-		seconds++;
-		setTime(seconds);
+	// let seconds = 0; variable muerta
+	setTimeout(() => {
+		// seconds++;
+		setTime(time + 1);
 		console.log(time);
 	}, 1000);
 
-	return <Counter seconds={seconds} />;
+	return <Counter seconds={time} />;
 };
--- src/js/component/Counter.js
+++ src/js/component/Counter.js
@@ -8,13 +8,23 @@ export const Counter = (props) => {
 			<div className="border border-3 rounded-3 d-inline p-2 me-3">
 				<FontAwesomeIcon icon={regular("clock")} inverse />
 			</div>
-			<div className="border border-3 rounded-3 d-inline p-2 me-3">0</div>
-			<div className="border border-3 rounded-3 d-inline p-2 me-3">0</div>
-			<div className="border border-3 rounded-3 d-inline p-2 me-3">0</div>
-			<div className="border border-3 rounded-3 d-inline p-2 me-3">0</div>
-			<div className="border border-3 rounded-3 d-inline p-2 me-3">0</div>
 			<div className="border border-3 rounded-3 d-inline p-2 me-3">
-				{props.seconds}
+				{Math.floor(props.seconds / 100000) % 1000000}
+			</div>
+			<div className="border border-3 rounded-3 d-inline p-2 me-3">
+				{Math.floor(props.seconds / 10000) % 100000}
+			</div>
+			<div className="border border-3 rounded-3 d-inline p-2 me-3">
+				{Math.floor(props.seconds / 1000) % 10000}
+			</div>
+			<div className="border border-3 rounded-3 d-inline p-2 me-3">
+				{Math.floor(props.seconds / 100) % 1000}
+			</div>
+			<div className="border border-3 rounded-3 d-inline p-2 me-3">
+				{Math.floor(props.seconds / 10) % 10}
+			</div>
+			<div className="border border-3 rounded-3 d-inline p-2 me-3">
+				{props.seconds % 10}
 			</div>
 		</div>
 	);
