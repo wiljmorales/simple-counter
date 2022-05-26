9d7364628a3ada96aeca42ca18607e0403031fbf
Wilfredo Morales <wiljmoralesa@gmail.com>
2022-05-26 01:56:39 +0000

    Starting challenge

---
+++ src/js/component/Counter.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const Counter = () => {
+	return (
+		<div>
+			<div>
+				<i class="fa-solid fa-clock"></i>
+			</div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+		</div>
+	);
+};
