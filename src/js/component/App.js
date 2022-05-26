19a58ab8c4a9230ae64682fb782bb347ee21877f
Wilfredo Morales <wiljmoralesa@gmail.com>
2022-05-26 03:17:59 +0000

    Got stuck here

--- src/js/component/App.js
+++ src/js/component/App.js
@@ -1,6 +1,15 @@
-import React from "react";
+import { counter } from "@fortawesome/fontawesome-svg-core";
+import React, { useState } from "react";
 import { Counter } from "./Counter";
 
 export const App = () => {
-	return <Counter />;
+	const [time, setTime] = useState(0);
+	let seconds = 0;
+	setInterval(() => {
+		seconds++;
+		setTime(seconds);
+		console.log(time);
+	}, 1000);
+
+	return <Counter seconds={seconds} />;
 };
--- src/js/component/Counter.js
+++ src/js/component/Counter.js
@@ -1,17 +1,21 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { regular } from "@fortawesome/fontawesome-svg-core/import.macro"; // <-- import styles to be used
 
-export const Counter = () => {
+export const Counter = (props) => {
 	return (
-		<div>
-			<div>
-				<i class="fa-solid fa-clock"></i>
+		<div className="bg-dark p-5 text-light fw-bold d-flex justify-content-center fs-4">
+			<div className="border border-3 rounded-3 d-inline p-2 me-3">
+				<FontAwesomeIcon icon={regular("clock")} inverse />
 			</div>
-			<div></div>
-			<div></div>
-			<div></div>
-			<div></div>
-			<div></div>
-			<div></div>
+			<div className="border border-3 rounded-3 d-inline p-2 me-3">0</div>
+			<div className="border border-3 rounded-3 d-inline p-2 me-3">0</div>
+			<div className="border border-3 rounded-3 d-inline p-2 me-3">0</div>
+			<div className="border border-3 rounded-3 d-inline p-2 me-3">0</div>
+			<div className="border border-3 rounded-3 d-inline p-2 me-3">0</div>
+			<div className="border border-3 rounded-3 d-inline p-2 me-3">
+				{props.seconds}
+			</div>
 		</div>
 	);
 };
