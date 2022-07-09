e029d2db5aef35f39b31548d68eba60903218699
Wilfredo Morales <wiljmoralesa@gmail.com>
2022-07-09 20:09:11 +0000

    Reviewing solution'

--- src/js/component/App.js
+++ src/js/component/App.js
@@ -3,11 +3,8 @@ import { Counter } from "./Counter";
 
 export const App = () => {
 	const [time, setTime] = useState(0);
-	// let seconds = 0; variable muerta
 	setTimeout(() => {
-		// seconds++;
 		setTime(time + 1);
-		console.log(time);
 	}, 1000);
 
 	return <Counter seconds={time} />;
